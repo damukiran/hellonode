5ff1126913fc18904d5753b2c4e8447a793d6064
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-08 14:19:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Running in local\n");
+  response.end("Running  7000 in local\n");
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("just updated successfully://127.0.0.1:8000/");
+server.listen(7000);
+console.log("just updated successfully://127.0.0.1:7000/");

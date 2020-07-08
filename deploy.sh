fd5e7571e95c717fa7d1502f2b872c0e96dabbc1
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-08 16:44:18 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-    git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
     . ~/.nvm/nvm.sh
     nvm install v10.11.0
     npm install
-    npm start
     npm install -g nodemon pm2
     pm2 restart ecosystem.config.js
     exit

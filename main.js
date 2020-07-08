6e5de1e6ad094075510234fda5695070f0c34d92
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-08 13:51:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ var server = http.createServer(function (request, response) {
 });
 
 // listen on localhost:8000
-server.listen(7000);
+server.listen(8000);
 console.log("updating now successfully://127.0.0.1:7000/");

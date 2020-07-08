1001f1b530032468a578bd7c9b849b00c164a48c
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-08 16:29:03 +0530

    Update gitscript.sh

--- gitscript.sh
+++ gitscript.sh
@@ -1,3 +1,4 @@
-cd hellonode && git checkout master && git pull origin master && npm i && npm install -g nodemon pm2 && pm2 restart ecosystem.config.js
+cd hellonode && git checkout master && git pull origin master && curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
+    . ~/.nvm/nvm.sh && nvm install v10.11.0 && npm install && npm install -g nodemon pm2 && pm2 restart ecosystem.config.js
 
 

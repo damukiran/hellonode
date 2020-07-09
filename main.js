d43135bb263a05c3ae62cfdc47e99722b3138e36
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-09 16:08:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Running with restarted server  in local\n");
+  response.end("Now showing demo\n");
 });
 
 // listen on localhost:8000

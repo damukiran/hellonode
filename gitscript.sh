bda29b0bc841a75c205ee5e07f46132bd4ed8327
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-09 20:32:34 +0530

    Update gitscript.sh

--- gitscript.sh
+++ gitscript.sh
@@ -1,2 +1,2 @@
-cd hellonode && git checkout master && git pull origin master && forever stop main.js && forever start main.js
+cd hellonode && git checkout master && git pull origin master && forever restart main.js
 

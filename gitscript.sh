1b3527b39e7773733765e467ce6dd075b606ab52
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-09 20:00:39 +0530

    Update gitscript.sh

--- gitscript.sh
+++ gitscript.sh
@@ -1,2 +1,2 @@
-cd hellonode && git checkout master && git pull origin master && sudo chown -R $USER /usr/local/lib/node_modules && sudo npm i -g pm2 && pm2 restart ecosystem.config.js
+cd hellonode && git checkout master && git pull origin master && sudo npm i -g pm2 && pm2 restart ecosystem.config.js
 

9ad86de04d2c272c86d36d932675ed69d050940e
damukiran <48375630+damukiran@users.noreply.github.com>
2020-07-09 11:56:29 +0530

    Update gitscript.sh

--- gitscript.sh
+++ gitscript.sh
@@ -1,2 +1,2 @@
-cd hellonode && git checkout master && git pull origin master && pm2 restart ecosystem.config.js
+cd hellonode && git checkout master && git pull origin master && npm install -g nodemon pm2 && pm2 restart ecosystem.config.js
 

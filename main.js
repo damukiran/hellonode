c34cd11c1b0c2a2a04856ca46affe5ddbeb9561d
damukiran <48375630+damukiran@users.noreply.github.com>
2020-10-22 20:49:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const https = require('https');
+const https = require('http');
 const fs = require('fs');
 
-const options = {
-  key: fs.readFileSync('../key.pem'),
-  cert: fs.readFileSync('../cert.pem')
-};
+// const options = {
+//   key: fs.readFileSync('../key.pem'),
+//   cert: fs.readFileSync('../cert.pem')
+// };
 
-https.createServer(options, function (req, res) {
+http.createServer( function (req, res) {
   res.writeHead(200);
   res.end("hello world\n");
-console.log('connected with port 8080')
-}).listen(8080);
+console.log('connected with port 6000')
+}).listen(6000);
